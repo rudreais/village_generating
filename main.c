e7a4c4b6bf402685941a1c09906e4252ce01456b
rudreais <valentin.lattay@rudreais.com>
2018-10-09 01:18:44 +0200

    allez, au dodo

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "vill_gen.h"
+
+int main(void)
+{
+	printf("dsl je me couche maintenant\n");
+	return 0;
+}
---
+++ vill_gen.h
@@ -0,0 +1,9 @@
+#ifndef VILL_GEN_H_
+#define VILL_GEN_H
+
+#pragma once
+
+typedef struct {
+} xx;
+
+#endif /* VILL_GEN_H_ */
