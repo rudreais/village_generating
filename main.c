c157a4bb5421c4e343df1442b76450e461a1d0bb
rudreais <valentin.lattay@rudreais.com>
2018-10-09 14:45:11 +0200

    structures added

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 
 int main(void)
 {
+	node main = {.parent = NULL, .coord = {.x = 4, .y = 4}};
+	
+	printf("%f\n%f\n", main.coord.x, main.coord.y);
 	printf("dsl je me couche maintenant\n");
 	return 0;
 }
--- vill_gen.h
+++ vill_gen.h
@@ -3,7 +3,24 @@
 
 #pragma once
 
+#include <SFML/Graphics.h>
+
 typedef struct {
-} xx;
+	float min;
+	float max;
+} interval2f;
+
+typedef struct {
+	unsigned int nb_slices;
+	interval2f lens;
+	interval2f childs;
+} generator_props;
+
+typedef struct node_params node;
+
+struct node_params {
+	node *parent;
+	sfVector2f coord;
+};
 
 #endif /* VILL_GEN_H_ */
