39df784476ddf3a73bc4a736695800d418a809a6
rudreais <valentin.lattay@rudreais.com>
2018-10-09 20:35:07 +0200

    basic functions added

--- main.c
+++ main.c
@@ -1,11 +1,35 @@
 #include <stdio.h>
 #include "vill_gen.h"
 
+void init_generator_props(generator_props *props)
+{
+	*props = (generator_props) {
+		.nb_slices = 5,
+		.lens = {
+			.min = 1,
+			.max = 5
+		},
+		.childs = {
+			.min = 4,
+			.max = 6
+		}
+	};
+}
+
+void run_generator(generator_props *props)
+{
+}
+
+void render_houses(void)
+{
+}
+
 int main(void)
 {
 	node main = {.parent = NULL, .coord = {.x = 4, .y = 4}};
+	generator_props props;
 	
-	printf("%f\n%f\n", main.coord.x, main.coord.y);
-	printf("dsl je me couche maintenant\n");
+	init_generator_props(&props);
+	printf("nb_slices=%d\n", props.nb_slices);
 	return 0;
 }
